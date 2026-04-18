05447e2be1da60aee4d369f20524ee58676d0dbd
Cristiano Salles <cristianosalles2011@gmail.com>
2026-04-18 17:51:38 -0300

    Resuming the project, recreating everything that had already been created to revisit and adapt some things.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_FORCE = -400.0
 
-var gravity = 1000
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
